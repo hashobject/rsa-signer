94ded3019241d0e3b73eaf470fe9b2fdc4f939c2
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-22 12:35:46 -0600

    initial implementation

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :url "https://github.com/hashobject/rsa-signer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.bouncycastle/bctsp-jdk16 "1.46"]])
--- src/rsa_signer/core.clj
+++ src/rsa_signer/core.clj
@@ -1,6 +1,41 @@
-(ns rsa-signer.core)
+(ns rsa-signer.core
+  "Library for signing strings with private RSA keys.")
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(java.security.Security/addProvider
+ (org.bouncycastle.jce.provider.BouncyCastleProvider.))
+
+(defn- read-key [f]
+  (-> f
+      java.io.FileReader.
+      org.bouncycastle.openssl.PEMReader.
+      .readObject))
+
+
+(defn- sign-bytes-with-private-key [data private-key algorithm]
+  (let [sig (doto (java.security.Signature/getInstance algorithm "BC")
+              (.initSign private-key (java.security.SecureRandom.))
+              (.update data))]
+    (.sign sig)))
+
+
+(defn- rsa-key [filepath]
+  (read-key (java.io.File. filepath)))
+
+
+(defn sign [s private-key-file-path algorithm]
+  "Sign input string `s` with private RSA key
+   located at path `private-key-file-path`
+   according to the algorithm.
+   Algorithm can be one of the following:
+    - RSA - that means that input string will be signed directly with RSA private key.
+    - SHA1withRSA - that means than input string will be hashed using SHA-1 and than signed with RSA private key.
+    - SHA256withRSA - that means than input string will be hashed using SHA-1 and than signed with RSA private key.
+   Method returns signed UTF-8 string.
+  "
+  (sign-bytes-with-private-key
+     (.getBytes s)
+     (.getPrivate (rsa-key private-key-file-path))
+     algorithm))
+
+
+
