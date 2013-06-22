1a3925a17f2efe701016afea34be7dd995c4ee2b
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-22 12:24:32 -0600

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rsa-signer
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/rsa_signer/core_test.clj
@@ -0,0 +1,7 @@
+(ns rsa-signer.core-test
+  (:use clojure.test
+        rsa-signer.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
